1942c6d7d02f8ddb8aa440932810bbbf0bce3d58
João Fraga <jgfraga@gmail.com>
2015-05-14 18:26:11 -0300

    Added MockUser features

--- lib/statistik/mock_user.rb
+++ lib/statistik/mock_user.rb
@@ -1,4 +1,25 @@
 module Statistik
-  class MockUser
+  class MockUser < Base
+
+    setup_delegators :@struct, :user_id,
+                               :mock_id,
+                               :answers_count,
+                               :answers_correct_count,
+                               :answers_correct_rate,
+                               :answers_comparison_rate,
+                               :comparison
+
+    relationship :comparison
+
+    class << self
+
+      def find(mock_id, user_id, options = {})
+        request 'mocks/:mock_id/users/:user_id',
+                {mock_id: mock_id, user_id: user_id},
+                options
+      end
+
+    end
+
   end
 end
