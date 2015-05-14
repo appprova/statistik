d90dcbda6b6827f17bc3183196862f3e56c50cce
João Fraga <jgfraga@gmail.com>
2015-05-14 18:11:25 -0300

    Added MockAlternative find object

--- lib/statistik/mock_alternative.rb
+++ lib/statistik/mock_alternative.rb
@@ -1,4 +1,23 @@
 module Statistik
-  class MockAlternative
+  class MockAlternative < Base
+
+    setup_delegators :@struct, :mock_id,
+                               :alternative_id,
+                               :answers_count,
+                               :answers_correct_count,
+                               :answers_correct_rate,
+                               :comparison,
+                               :school
+
+    relationship :comparison
+    relationship :school
+
+    class << self
+      def find(mock_id, alternative_id, options = {})
+        request 'mocks/:mock_id/alternatives/:alternative_id', 
+                {mock_id: mock_id, alternative_id: alternative_id}, 
+                options
+      end
+    end
   end
 end
