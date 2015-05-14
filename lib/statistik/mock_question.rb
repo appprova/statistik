c6606763e2cd7f17f39ba99c824f51b04164df8f
João Fraga <jgfraga@gmail.com>
2015-05-14 18:25:54 -0300

    Added MockQuestion features

--- lib/statistik/mock_question.rb
+++ lib/statistik/mock_question.rb
@@ -1,4 +1,25 @@
 module Statistik
-  class MockQuestion
+  class MockQuestion < Base
+
+    setup_delegators :@struct, :mock_id,
+                               :question_id,
+                               :answers_count,
+                               :answers_correct_count,
+                               :answers_correct_rate,
+                               :comparison,
+                               :school
+
+    relationship :comparison
+    relationship :school
+
+    class << self
+
+      def find(mock_id, question_id, options = {})
+        request 'mocks/:mock_id/questions/:question_id',
+                {mock_id: mock_id, question_id: question_id},
+                options
+      end
+
+    end
   end
 end
