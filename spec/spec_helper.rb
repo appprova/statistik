4e47635704fc2619eb0529b2f37e84798628c6b7
João Fraga <jgfraga@gmail.com>
2015-05-14 16:46:02 -0300

    Added test fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,23 @@ require 'pp'
 
 include Statistik
 Statistik::Client.config 
+
+FIXTURES_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
+ROOT_URL = [Statistik::Client::URL.chomp('/'), Statistik::Client::API_VERSION].join '/'
+FakeWeb.allow_net_connect = false
+
+def register(options)
+  url = api_method_url(options[:url])
+  FakeWeb.register_uri(:get, url, :body => read_fixture(options[:body] + '.json'))
+end
+
+def read_fixture(fixture)
+  File.read(File.join(FIXTURES_PATH, fixture))
+end
+
+def api_method_url(method)
+  ROOT_URL + method
+end
+
+register(:url => 'mocks/1234', :body => 'mocks_by_id')
+register(:url => 'mocks/1234?school_id=123', :body => 'mocks_by_id_with_comparison')
--- spec/statistik/mock_spec.rb
+++ spec/statistik/mock_spec.rb
@@ -4,13 +4,12 @@ describe Statistik::Mock do
   it 'should return a mock statistic identified by its id (without school)' do
     mock = Statistik::Mock.find 1234
 
-    expect(mock.mock_id).to eql(value)
-    expect(mock.mock_id).to eql(1234)
-    expect(mock.questions_count).to eql(5)
-    expect(mock.answers_count).to eql(20)
-    expect(mock.answers_correct_count).to eql(10)
-    expect(mock.answers_correct_rate).to eql(0.5)
-    expect(mock.users_count).to eql(4)
+    expect(mock.mock_id).to eq(1234)
+    expect(mock.questions_count).to eq(5)
+    expect(mock.answers_count).to eq(20)
+    expect(mock.answers_correct_count).to eq(10)
+    expect(mock.answers_correct_rate).to eq(0.5)
+    expect(mock.users_count).to eq(4)
 
     expect(mock.comparison).to be_nil
     expect(mock.school).to be_nil
@@ -22,15 +21,15 @@ describe Statistik::Mock do
     expect(mock.comparison).to be_instance_of Statistik::MockComparison
     expect(mock.school).to be_instance_of Statistik::MockSchool
 
-    expect(mock.comparison.answers_count).to eql(3)
-    expect(mock.comparison.answers_correct_count).to eql(3)
-    expect(mock.comparison.answers_correct_rate).to eql(1)
-    expect(mock.comparison.users_count).to eql(1)
+    expect(mock.comparison.answers_count).to eq(3)
+    expect(mock.comparison.answers_correct_count).to eq(3)
+    expect(mock.comparison.answers_correct_rate).to eq(1)
+    expect(mock.comparison.users_count).to eq(1)
     
-    expect(mock.school.school_id).to eql(123)
-    expect(mock.school.answers_count).to eql(2)
-    expect(mock.school.answers_correct_count).to eql(2)
-    expect(mock.school.answers_correct_rate).to eql(1)
-    expect(mock.school.users_count).to eql(1)
+    expect(mock.school.school_id).to eq(123)
+    expect(mock.school.answers_count).to eq(2)
+    expect(mock.school.answers_correct_count).to eq(2)
+    expect(mock.school.answers_correct_rate).to eq(1)
+    expect(mock.school.users_count).to eq(1)
   end
 end
